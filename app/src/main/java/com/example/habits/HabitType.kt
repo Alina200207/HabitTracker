8c6c9dce9ac0db4886e71a0a0107d8a01e969303
Alina200207 <alina_m2002@list.ru>
2023-04-03 22:55:00 +0500

    refactoring

--- app/src/main/java/com/example/habits/HabitType.kt
+++ app/src/main/java/com/example/habits/HabitType.kt
@@ -4,15 +4,4 @@ import androidx.annotation.StringRes
 
 enum class HabitType(@StringRes val text: Int, @StringRes val enum_text: Int) {
     Good(R.string.good_habit, R.string.good_enum), Bad(R.string.bad_habit, R.string.bad_enum);
-
-    companion object {
-
-        fun toEnum(type: String): HabitType {
-            return when (type) {
-                Constants.goodTypeText -> Good
-                Constants.badTypeText -> Bad
-                else -> Good
-            }
-        }
-    }
 }
